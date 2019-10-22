ebf81c37d55c8982f5296cf2a955bdfc7d4f25e5
Sambor Górnicz <samborgornicz@MacBook-Pro-Sambor.local>
2019-10-22 17:39:47 +0200

    Add tests for utils file

---
+++ __tests__/utils.js
@@ -0,0 +1,31 @@
+import { getRtmUrl, validateConfig } from "../src/utils";
+
+describe("Test utlis", () => {
+  test("validateConfig", () => {
+    const validConfig = { account_token: "token" };
+    const invalidConfig = { region: "europe" };
+
+    // Valid case
+    expect(validateConfig(validConfig)).toBe(true);
+
+    // Invalid cases
+    expect(validateConfig).toThrow(new Error("ChatSDK: Missing config"));
+    expect(() => validateConfig(invalidConfig)).toThrow(
+      new Error("ChatSDK: Incorrect config, missing 'account_token' value")
+    );
+  });
+
+  test("getRtmUrl", () => {
+    const versionRegex = new RegExp(/\/v([\d\.]*)\//);
+    const newApiVersion = "1.1";
+
+    expect(getRtmUrl()).toEqual(process.env.CHAT_RTM_URL);
+    expect(getRtmUrl({ region: "america" })).toEqual(process.env.CHAT_RTM_URL);
+    expect(getRtmUrl({ region: "europe" })).toEqual(
+      process.env.EUROPE_CHAT_RTM_URL
+    );
+
+    const urlWithUpdatedVersion = getRtmUrl({ apiVersion: newApiVersion });
+    expect(urlWithUpdatedVersion.match(versionRegex)[1]).toEqual(newApiVersion);
+  });
+});
