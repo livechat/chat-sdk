decbd5b9f2b6370d45a0610174fe7cf1f6cd5b3e
Sambor Górnicz <samborgornicz@MacBook-Pro-Sambor.local>
2019-10-22 12:14:55 +0200

    Add utils file

---
+++ src/utils.js
@@ -0,0 +1,36 @@
+export function debounce(func, timeout) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func(...args);
+    }, timeout);
+  };
+}
+
+export function validateConfig(config) {
+  if (!config) {
+    throw new Error("ChatSDK: Missing config");
+  } else if (!config.account_token) {
+    throw new Error("ChatSDK: Incorrect config, missing 'account_token' value");
+  } else {
+    return true;
+  }
+}
+
+export function getRtmUrl({ region, apiVersion }) {
+  const versionRegex = new RegExp(/\/v([\d\.]*)\//);
+
+  let url =
+    region && region.toLowerCase() === "europe"
+      ? process.env.EUROPE_CHAT_RTM_URL
+      : process.env.CHAT_RTM_URL;
+
+  if (apiVersion)
+    url = url.replace(versionRegex, () => {
+      const shouldIncludeVersionTag = !apiVersion.includes("v");
+      return `/${shouldIncludeVersionTag ? "v" : ""}${apiVersion}/`;
+    });
+
+  return url;
+}
