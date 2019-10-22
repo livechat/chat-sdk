d5422318a17422daf7813e8a7db56261b928be7e
Sambor Górnicz <samborgornicz@MacBook-Pro-Sambor.local>
2019-10-22 12:13:44 +0200

    Add constants file

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+// RTM API
+export const LOGIN = "login";
+export const SEND_EVENT = "send_event";
+
+// WEBSOCKET:
+export const CONNECT = "connect";
+export const DISCONNECT = "disconnect";
+export const OPEN = "open";
+export const CLOSE = "close";
+export const MESSAGE = "message";
+export const HEARTBEAT = "heartbeat";
+export const PING = "ping";
