fd15a2e9d2183221a328ac9469d61317607875f1
Sambor Górnicz <samborgornicz@MacBook-Pro-Sambor.local>
2019-10-22 17:37:58 +0200

    Add husky and eslint configs

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ["standard"],
+  parser: "babel-eslint",
+  rules: {
+    "camelcase": 0,
+    "semi": 0,
+    "space-before-function-paren": 0,
+    "quotes": ["error", "double"],
+  }
+};
