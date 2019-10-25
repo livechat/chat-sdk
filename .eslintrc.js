079653178c4f8f31090e9ed1146e8bda6836cd1d
Sambor Górnicz <sambor@g.pl>
2019-10-25 17:16:41 +0200

    Add mock for sockjs-client library

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,5 +6,14 @@ module.exports = {
     "semi": 0,
     "space-before-function-paren": 0,
     "quotes": ["error", "double"],
-  }
+  },
+  overrides: [
+    {
+      files: ["__tests__/*.js", "__mocks__/*.js"],
+      rules: {
+        "no-unused-expressions": "off",
+        "no-undef": 0
+      }
+    }
+  ]
 };
